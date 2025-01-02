d1c0e5cf3d0ad1f2e655a22c5b07cfcfb598e13e
Anurags32 <anuragtiwaris1172000@gmail.com>
2025-01-02 11:05:46 +0530

    celebritycontroller update

--- lib/controler/celebrity_controler.dart
+++ lib/controler/celebrity_controler.dart
@@ -21,24 +21,16 @@ class CelibrityList extends GetxController {
       _isLoading.value = true;
       SharedPreferences prefs = await SharedPreferences.getInstance();
       String? token = prefs.getString('token');
-
       final url = '$apiUrl/list-celebrity';
-
       final Dio dio = Dio();
       dio.options.headers['Authorization'] = 'Bearer $token';
-
       final response = await dio.get(
         url,
       );
-
       if (response.statusCode == 201) {
         final responseData = response.data;
-
-        print(responseData);
-
         if (responseData != null && responseData['status'] == true) {
           final dataList = responseData['data'];
-          print(dataList);
           if (dataList != null && dataList is List) {
             List<Data> parsedDataList =
                 dataList.map((item) => Data.fromJson(item)).toList();
