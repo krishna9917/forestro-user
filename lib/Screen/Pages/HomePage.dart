2a5210d1f2a0b4432d04511402e24c0b183d2ef7
Anurags32 <anuragtiwaris1172000@gmail.com>
2025-01-04 15:57:20 +0530

    version update 23

--- lib/Screen/Pages/HomePage.dart
+++ lib/Screen/Pages/HomePage.dart
@@ -264,7 +264,7 @@ class _HomePageState extends State<HomePage> {
                 child: Badge(
                   backgroundColor: AppColor.primary,
                   label: const Text(
-                    "2",
+                    "23",
                     style: TextStyle(fontSize: 10),
                   ),
                   child: SvgPicture.asset(
